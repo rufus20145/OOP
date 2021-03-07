c92fdcbdd448de4eee87593b13551d7aad7fc63d
LAPTOP20145 <ivan20027749@gmail.com>
2021-03-07 13:32:54 +0300

    Закончил второе ДЗ за 20 минут

--- src/homeworks/second/Task3.java
+++ src/homeworks/second/Task3.java
@@ -1,4 +1,4 @@
-package src.homeworks.second;
+// package src.homeworks.second;
 
 /**
  * Вводится строка из произвольных символов. Необходимо вывести на консоль
@@ -6,9 +6,39 @@ package src.homeworks.second;
  * символов / кол-во символов).
  */
 
+import java.util.Scanner;
+
 public class Task3 {
 
     public static void main(String[] args) {
-        
+        String inputString = null;
+        float sumOfDigits = 0;
+        int numberOfDigits;
+        Scanner input = new Scanner(System.in);
+        boolean exceptionCaught = false;
+
+        do {
+            try {
+                inputString = input.nextLine();
+            } catch (java.util.NoSuchElementException e) {
+                System.out.println("You didn't enter any text. Try again.");
+                input.nextLine();
+                exceptionCaught = true;
+            } catch (java.lang.IllegalStateException e) {
+                System.out.println("Something went wrong. Try again.");
+                input.nextLine();
+                exceptionCaught = true;
+            }
+            {
+
+            }
+        } while (exceptionCaught);
+        input.close();
+
+        for (char digit : inputString.toCharArray()) {
+            sumOfDigits += (int) digit;
+        }
+
+        System.out.println(sumOfDigits / inputString.length());
     }
 }
--- src/homeworks/second/Task4.java
+++ src/homeworks/second/Task4.java
@@ -1,4 +1,4 @@
-package src.homeworks.second;
+// package src.homeworks.second;
 
 /**
  * В консоль вводятся три числа с обязательной дробной частью. Необходимо найти
@@ -8,9 +8,46 @@ package src.homeworks.second;
  * 3) Разницу первой и второй найденных сумм
  */
 
+import java.util.Scanner;
+
 public class Task4 {
 
     public static void main(String[] args) {
-        
+
+        final int numberOfDigits = 3;
+
+        Scanner input = new Scanner(System.in);
+        float digit = 0;
+        float firstSum = 0;
+        int secondSum = 0;
+        float summDifference;
+        boolean exceptionCaught = false;
+
+        System.out.println("Enter " + numberOfDigits + " numbers.");
+        for (int i = 0; i < numberOfDigits; i++) {
+            do {
+                try {
+                    digit = input.nextFloat();
+                    exceptionCaught = false;
+                } catch (java.util.InputMismatchException e) {
+                    System.out.println("You must enter a valid float value. Try again.");
+                    exceptionCaught = true;
+                    input.nextLine();
+                } catch (java.util.NoSuchElementException e) {
+                    System.out.println("You didn't enter any text. Try again.");
+                    exceptionCaught = true;
+                    input.nextLine();
+                } catch (java.lang.IllegalStateException e) {
+                    System.out.println("Scanner was closed before input. Try again.");
+                    input.nextLine();
+                    exceptionCaught = true;
+                }
+            } while (exceptionCaught);
+            firstSum += digit;
+            secondSum += digit;
+        }
+        System.out.println(firstSum);
+        System.out.println(secondSum);
+        System.out.println(firstSum - secondSum);
     }
 }
