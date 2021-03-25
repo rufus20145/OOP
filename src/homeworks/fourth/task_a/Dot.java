77c4e9a7125c640e41e2bb440c39dbdb5f21499c
RUFUS20145 <ivan20027749@gmail.com>
2021-03-25 10:21:46 +0300

    сделал одну задачку из дз4

---
+++ src/homeworks/fourth/task_a/Dot.java
@@ -0,0 +1,79 @@
+package src.homeworks.fourth.task_a;
+
+import java.util.Locale;
+
+public class Dot {
+
+    private double x;
+    private double y;
+    private double z;
+
+    public Dot() {
+
+    }
+
+    public Dot(boolean makeRandom) {
+
+        this(Math.random() * 1000, Math.random() * 1000, Math.random() * 1000);
+
+        StringBuilder tempStringBuilder = new StringBuilder("Random dot was generated with such coordinates:");
+        tempStringBuilder.append(String.format(Locale.PRC, "%.4f", x)).append(", ").append(String.format("%.4f", y))
+                .append(", ").append(String.format("%.4f", z)).append(".");
+        System.out.println(tempStringBuilder.toString());
+
+    }
+
+    public Dot(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public static double calcTheDistance(Dot firstDot, Dot secondDot) {
+        return Math.sqrt(Math.pow(firstDot.getX() - secondDot.getX(), 2)
+                + Math.pow(firstDot.getY() - secondDot.getY(), 2) + Math.pow(firstDot.getZ() - secondDot.getZ(), 2));
+    }
+
+    public void setCoordinate(double coordinate, int numberOfDimension) {
+
+        switch (numberOfDimension) {
+        case 1:
+            this.x = coordinate;
+            break;
+        case 2:
+            this.y = coordinate;
+            break;
+        case 3:
+            this.z = coordinate;
+            break;
+        default:
+            System.out.println("Неверный номер измерения");
+            break;
+        }
+    }
+
+    public String getAllInfo() {
+        StringBuilder output = new StringBuilder(String.format(Locale.PRC, "x = %.2f, y = %.2f, z = %.2f", x, y, z));
+        return output.toString();
+    }
+
+    public String getCoordinates() {
+        StringBuilder temp = new StringBuilder();
+        temp.append("(").append(String.format("%.0f", x)).append(", ").append(String.format("%.0f", y)).append(", ")
+                .append(String.format("%.0f", z)).append(") ");
+                return temp.toString();
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+}
---
+++ src/homeworks/fourth/task_a/Main.java
@@ -0,0 +1,111 @@
+package src.homeworks.fourth.task_a;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        final String stringFormatRegex = "^\\(-?\\d+\\,\\ ?-?\\d+\\,\\ ?-?\\d+\\)$";
+
+        int numberOfDots = 0;
+        Dot[] dots;
+        Dot minDot;
+        double minSum = 0;
+        boolean exceptionCaught = false;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Введите количество точек: ");
+        do {
+            try {
+                numberOfDots = input.nextInt();
+                if (numberOfDots > 0) {
+                    input.nextLine();
+                    exceptionCaught = false;
+                } else {
+                    System.out.println("Количество точек должно быть положительным числом.");
+                    exceptionCaught = true;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Здесь можно ввести только целое число. Повторите попытку.");
+                exceptionCaught = true;
+                input.nextLine();
+            } catch (NoSuchElementException e) {
+                System.out.println("Вы не ввели ничего. Повторите попытку.");
+                exceptionCaught = true;
+                input.nextLine();
+            } catch (IllegalStateException e) {
+                System.out.println("Система ввода оказалась в некорректном состоянии. Повторите попытку.");
+                exceptionCaught = true;
+                input = new Scanner(System.in);
+                input.nextLine();
+            }
+        } while (exceptionCaught);
+        dots = new Dot[numberOfDots];
+        dots[0] = new Dot();
+
+        for (int currDot = 0; currDot < numberOfDots; ++currDot) {
+            String lineToParse = null;
+            do {
+                try {
+                    lineToParse = input.nextLine();
+                    if (!lineToParse.matches(stringFormatRegex)) {
+                        System.out.println(
+                                "Строка должна соответствовать формату (1,2,3) или (1, 2, 3). Повторите попытку");
+                        exceptionCaught = true;
+                    } else {
+                        exceptionCaught = false;
+                    }
+                } catch (NoSuchElementException e) {
+                    System.out.println("Вы не ввели ничего. Повторите попытку.");
+                    exceptionCaught = true;
+                    input.nextLine();
+                } catch (IllegalStateException e) {
+                    System.out.println("Система ввода оказалась в некорректном состоянии. Повторите попытку.");
+                    exceptionCaught = true;
+                    input = new Scanner(System.in);
+                    input.nextLine();
+                }
+                if (!exceptionCaught) {
+                    dots[currDot] = new Dot();
+                    int numberOfDimension = 1;
+
+                    lineToParse = lineToParse.replace("(", "").replace(")", "").replace(" ", "");
+                    String[] splitStrings = lineToParse.split("\\,");
+
+                    for (String string : splitStrings) {
+                        dots[currDot].setCoordinate(Double.parseDouble(string), numberOfDimension);
+                        ++numberOfDimension;
+                    }
+                }
+            } while (exceptionCaught);
+            System.out.println(dots[currDot].getAllInfo());
+        }
+
+        minDot = dots[0];
+        if (numberOfDots > 1) {
+            for (int j = 0; j < dots.length; j++) {
+                minSum += Dot.calcTheDistance(dots[0], dots[1]);
+            }
+        }
+        for (int i = 1; i < dots.length; i++) {
+            double currSum = 0;
+            for (int j = 0; j < dots.length; j++) {
+                if (i != j) {
+                    currSum += Dot.calcTheDistance(dots[i], dots[j]);
+                }
+            }
+            if (currSum < minSum) {
+                minDot = dots[i];
+                minSum = currSum;
+            }
+        }
+
+        System.out.println(minDot.getCoordinates() + String.format(Locale.PRC, "%.2f", minSum));
+
+        input.close();
+    }
+}
