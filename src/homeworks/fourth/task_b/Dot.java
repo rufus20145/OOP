3af3817fb357aeb0ec4d276e14c8b1bae0c084d1
RUFUS20145 <ivan20027749@gmail.com>
2021-03-26 18:12:03 +0300

    закончил ДЗ4

---
+++ src/homeworks/fourth/task_b/Dot.java
@@ -0,0 +1,75 @@
+package src.homeworks.fourth.task_b;
+
+import java.util.Locale;
+
+public class Dot {
+
+    private double x;
+    private double y;
+
+    public Dot() {
+
+    }
+
+    public Dot(boolean makeRandom) {
+
+        this(Math.random() * 1000, Math.random() * 1000);
+
+        StringBuilder tempStringBuilder = new StringBuilder("Random dot was generated with such coordinates:");
+        tempStringBuilder.append(String.format(Locale.PRC, "%.4f", x)).append(", ").append(String.format("%.4f", y))
+                .append(".");
+        System.out.println(tempStringBuilder.toString());
+
+    }
+
+    public Dot(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * метод для подсчета расстояния между двумя точками на плоскости
+     * @param firstDot координаты первой точки 
+     * @param secondDot координаты второй точки
+     * @return расстояние между точками
+     */
+    public static double calcTheDistance(Dot firstDot, Dot secondDot) {
+        return Math.sqrt(Math.pow(firstDot.getX() - secondDot.getX(), 2)
+                + Math.pow(firstDot.getY() - secondDot.getY(), 2));
+    }
+
+    public void setCoordinate(double coordinate, int numberOfDimension) {
+
+        switch (numberOfDimension) {
+        case 1:
+            this.x = coordinate;
+            break;
+        case 2:
+            this.y = coordinate;
+            break;
+        default:
+            System.out.println("Неверный номер измерения");
+            break;
+        }
+    }
+
+    public String getAllInfo() {
+        StringBuilder output = new StringBuilder(String.format(Locale.PRC, "x = %.2f, y = %.2f", x, y));
+        return output.toString();
+    }
+
+    public String getCoordinates() {
+        StringBuilder temp = new StringBuilder();
+        temp.append("(").append(String.format("%.0f", x)).append(", ").append(String.format("%.0f", y)).append(") ");
+        return temp.toString();
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+}
---
+++ src/homeworks/fourth/task_b/Main.java
@@ -0,0 +1,118 @@
+package src.homeworks.fourth.task_b;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        final String stringFormatRegex = "^\\(-?\\d+\\,\\ ?-?\\d+\\)$";
+
+        int numberOfDots = 0;
+        Dot[] dots;
+        boolean exceptionCaught = false;
+        double maxArea = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Введите количество точек: ");
+        do {
+            try {
+                numberOfDots = input.nextInt();
+                if (numberOfDots > 2) {
+                    input.nextLine();
+                    exceptionCaught = false;
+                } else {
+                    System.out.println("Минимальное количество точек - 3, т.к. необходимо построить треугольник.");
+                    exceptionCaught = true;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Здесь можно ввести только целое число. Повторите попытку.");
+                exceptionCaught = true;
+                input.nextLine();
+            } catch (NoSuchElementException e) {
+                System.out.println("Вы не ввели ничего. Повторите попытку.");
+                exceptionCaught = true;
+                input.nextLine();
+            } catch (IllegalStateException e) {
+                System.out.println("Система ввода оказалась в некорректном состоянии. Повторите попытку.");
+                exceptionCaught = true;
+                input = new Scanner(System.in);
+                input.nextLine();
+            }
+        } while (exceptionCaught);
+        dots = new Dot[numberOfDots];
+
+        for (int currDot = 0; currDot < numberOfDots; ++currDot) {
+            String lineToParse = null;
+            do {
+                try {
+                    lineToParse = input.nextLine();
+                    if (!lineToParse.matches(stringFormatRegex)) {
+                        System.out.println("Строка должна соответствовать формату (1,2) или (1, 2). Повторите попытку");
+                        exceptionCaught = true;
+                    } else {
+                        exceptionCaught = false;
+                    }
+                } catch (NoSuchElementException e) {
+                    System.out.println("Вы не ввели ничего. Повторите попытку.");
+                    exceptionCaught = true;
+                    input.nextLine();
+                } catch (IllegalStateException e) {
+                    System.out.println("Система ввода оказалась в некорректном состоянии. Повторите попытку.");
+                    exceptionCaught = true;
+                    input = new Scanner(System.in);
+                    input.nextLine();
+                }
+                if (!exceptionCaught) {
+                    dots[currDot] = new Dot();
+                    int numberOfDimension = 1;
+
+                    lineToParse = lineToParse.replace("(", "").replace(")", "").replace(" ", "");
+                    String[] splitStrings = lineToParse.split("\\,");
+
+                    for (String string : splitStrings) {
+                        dots[currDot].setCoordinate(Double.parseDouble(string), numberOfDimension);
+                        ++numberOfDimension;
+                    }
+                }
+            } while (exceptionCaught);
+            // System.out.println(dots[currDot].getAllInfo());//вывод всех данных точки после парсинга
+        }
+
+        for (int i = 0; i < numberOfDots; i++) {
+            for (int j = 0; j < numberOfDots; j++) {
+                for (int k = 0; k < numberOfDots; k++) {
+                    double currArea = computeAreaOfTriangle(dots[i], dots[j], dots[k]);
+                    if (currArea > maxArea) {
+                        maxArea = currArea;
+                    }
+                }
+            }
+        }
+
+        System.out.println(String.format(Locale.PRC, "%.2f", maxArea));
+
+        input.close();
+    }
+
+    /**
+     * @param firstDot  координаты первой точки треугольника
+     * @param secondDot координаты второй точки треугольника
+     * @param thirdDot  координаты третьей точки треугольника
+     * @return площадь треугольника
+     */
+    public static double computeAreaOfTriangle(Dot firstDot, Dot secondDot, Dot thirdDot) {
+
+        double firstEdge = Dot.calcTheDistance(firstDot, secondDot);
+        double secondEdge = Dot.calcTheDistance(secondDot, thirdDot);
+        double thirdEdge = Dot.calcTheDistance(thirdDot, firstDot);
+
+        double semiPerimeter = (firstEdge + secondEdge + thirdEdge) / 2;
+
+        return Math.sqrt(semiPerimeter * (semiPerimeter - firstEdge) * (semiPerimeter - secondEdge)
+                * Math.abs((semiPerimeter - thirdEdge)));
+    }
+}
