58f24994ad867461f81c35055c67449ffc940894
RUFUS20145 <ivan20027749@gmail.com>
2021-04-06 17:13:38 +0300

    Первая задачка из ДЗ 6

--- src/homeworks/sixth/TaskA.java
+++ src/homeworks/sixth/TaskA.java
@@ -1,7 +1,7 @@
 package src.homeworks.sixth;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
@@ -10,58 +10,136 @@ public class TaskA {
         final String NoSuchElementExceptionErrorMessage = "Вы ничего не ввели. Выход из программы.";
         final String IllegalStateExceptionErrorMessage = "Система ввода оказалась в некорректном состоянии. Повторите попытку.";
 
-        final String firstCollectionRegex = "^STREAM 1$";
-        final String secondCollectionRegex = "^STREAM 2$";
-        final String thirdCollectionRegex = "^STREAM 3$";
-        final String endRegex = "^END$";
+        final String numberRegex = "^-?\\d+$";
+        final String streamRegex = "^STREAM \\d$";
+        final String firstCollectionSwitch = "STREAM 1";
+        final String secondCollectionSwitch = "STREAM 2";
+        final String thirdCollectionSwitch = "STREAM 3";
+        final String endSwitch = "END";
 
         ArrayList<Integer> firstList = new ArrayList<>();
         ArrayList<Integer> secondList = new ArrayList<>();
         ArrayList<Integer> thirdList = new ArrayList<>();
-        ArrayList<Integer> resultList = new ArrayList<>();
+        int[] resultArray;
         int currentStream = 1;
+        int maxNumberOfElements = 0;
         Scanner input = new Scanner(System.in);
+        StringBuilder output = new StringBuilder();
         boolean exceptionCaught = false;
         boolean isInLoop = true;
+        boolean isFirstIteration = true;
+
 
         do {
-            String inputLine = null;
+            String scannedLine = null;
 
-            switch (currentStream) {
-            case 1: {
-                do {
-                    exceptionCaught = false;
-                    try {
-                        inputLine = input.nextLine();
-                    } catch (NoSuchElementException e) {
-                        System.out.println(NoSuchElementExceptionErrorMessage);
-                        input.nextLine();
-                        exceptionCaught = true;
-                    } catch (IllegalStateException e) {
-                        System.out.println(IllegalStateExceptionErrorMessage);
-                        input = new Scanner(System.in);
-                        input.nextLine();
-                        exceptionCaught = true;
-                    }
-                } while (exceptionCaught);
-                
-                break;
-            }
-            case 2: {
-                System.out.println("2");
-                break;
-            }
-            case 3: {
-                System.out.println("3");
-                break;
-            }
+            do {
+                exceptionCaught = false;
+                try {
+                    scannedLine = input.nextLine();
+                } catch (NoSuchElementException e) {
+                    System.out.println(NoSuchElementExceptionErrorMessage);
+                    input.nextLine();
+                    exceptionCaught = true;
+                } catch (IllegalStateException e) {
+                    System.out.println(IllegalStateExceptionErrorMessage);
+                    input = new Scanner(System.in);
+                    input.nextLine();
+                    exceptionCaught = true;
+                }
+            } while (exceptionCaught);
 
-            default: {
-                System.out.println("Default");
-                break;
-            }
+            // данный блок не должен выполняться сразу после запуска, значения идут в
+            // первую коллекцию
+            if (!isFirstIteration && scannedLine.matches(streamRegex) || scannedLine.matches(endSwitch)) {
 
+                switch (scannedLine) {
+                case firstCollectionSwitch: {
+                    //System.out.println("Выбран 1 поток.");
+                    currentStream = 1;
+                    break;
+                }
+                case secondCollectionSwitch: {
+                    //System.out.println("Выбран 2 поток.");
+                    currentStream = 2;
+                    break;
+                }
+                case thirdCollectionSwitch: {
+                    //System.out.println("Выбран 3 поток.");
+                    currentStream = 3;
+                    break;
+                }
+                case endSwitch: {
+                    //System.out.println("Выход из программы");
+                    isInLoop = false;
+                    break;
+                }
+                default: {
+                    //System.out.println("Неправильный номер потока. Их всего 3. Номер не был изменен.");
+                    break;
+                }
+                }
+            } else if (scannedLine.matches(numberRegex)) {
+
+                switch (currentStream) {
+                case 1: {
+                    //System.out.println("Добавлено в 1 поток");
+
+                    firstList.add(Integer.parseInt(scannedLine));
+                    break;
+                }
+                case 2: {
+                    //System.out.println("Добавлено в 2 поток");
+
+                    secondList.add(Integer.parseInt(scannedLine));
+                    break;
+                }
+                case 3: {
+                    //System.out.println("Добавлено в 3 поток");
+
+                    thirdList.add(Integer.parseInt(scannedLine));
+                    break;
+                }
+
+                default: {
+                    //System.out.println("Неизвестный поток");
+                    break;
+                }
+
+                }
+            } else {
+                // System.out.println("Ввод не соответствует формату.");
             }
+            isFirstIteration = false;
         } while (isInLoop);
+
+        maxNumberOfElements = Math.max(firstList.size(), Math.max(secondList.size(), thirdList.size()));
+        resultArray = new int[maxNumberOfElements];
+
+        if (maxNumberOfElements == 0) {
+            //System.out.println("Коллекции пусты.");
+        } else {
+            for (int i = 0; i < maxNumberOfElements; i++) {
+
+                if (i < firstList.size()) {
+                    //System.out.println("Добавили элемент из 1 списка");
+                    resultArray[i] += firstList.get(i);
+                }
+                if (i < secondList.size()) {
+                    //System.out.println("Добавили элемент из 2 списка");
+                    resultArray[i] += secondList.get(i);
+                }
+                if (i < thirdList.size()) {
+                    //System.out.println("Добавили элемент из 3 списка");
+                    resultArray[i] += thirdList.get(i);
+                }
+                output.append(resultArray[i]).append(" ");
+
+            }
+
+            System.out.println(output.toString());
+        }
+
+        input.close();
     }
 }
