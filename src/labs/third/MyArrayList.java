28f2d85d2e399fb7d98bc18d26eeef15b1740cab
RUFUS20145 <ivan20027749@gmail.com>
2021-04-17 10:52:35 +0300

    начало 3 лабы

--- src/labs/third/MyArrayList.java
+++ src/labs/third/MyArrayList.java
@@ -1,8 +1,26 @@
 package src.labs.third;
 
-public class MyArrayList implements src.labs.third.interfaces.Array {
-    
-    public int size() {
-        return 1;
+import src.labs.third.interfaces.Array;
+public class MyArrayList implements Array {
+
+    private static final int DEFAULT_CAPACITY = 5;
+
+    private String[] array;
+    private int size;
+
+    public MyArrayList() {
+        this.array = new String[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayList(int initSize) {
+        if(initSize > 0) {
+            this.array = new String[initSize];
+        } else {
+            this.array = new String[DEFAULT_CAPACITY];
+        }
+    }
+
+    public boolean add(String o) {
+        return false;
     }
 }
