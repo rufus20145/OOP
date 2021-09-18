7c4a17a3bffae6b3e326bcd06b4d8f230e7ddbec
LAPTOP-20145 <ivan20027749@gmail.com>
2021-09-18 18:08:52 +0300

    а вот и зачёт сдался

---
+++ src/Zachet/Main.java
@@ -0,0 +1,99 @@
+package src.Zachet;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    private static final double FIRST_CONSTANT = 200; // верхнее ограничение для генерируемых чисел
+    private static final double SECOND_CONSTANT = -100; // смещение, чтобы числа были в том числе и отрицательными
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int matrixSize;
+        int[][] matrix;
+
+        System.out.println("Введите размер генерируемой матрицы");
+        matrixSize = in.nextInt();
+        matrix = new int[matrixSize][matrixSize];
+
+        fillMatrix(matrix, matrixSize);
+
+        int minDiagElement = findMinDiagElement(matrix);
+        System.out.println(minDiagElement);
+
+        int[][] newMatrix = removeRowsWithElement(matrix, minDiagElement);
+
+        System.out.println("Исходная матрица:");
+        printMatrix(matrix);
+
+        System.out.println("Полученная матрица");
+        printMatrix(newMatrix);
+
+        in.close();
+    }
+
+    private static void fillMatrix(int[][] matrix, int matrixSize) {
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                matrix[i][j] = (int) Math.round(FIRST_CONSTANT * Math.random() + SECOND_CONSTANT);
+            }
+        }
+    }
+
+    private static int findMinDiagElement(int[][] matrix) {
+        int minElement = matrix[0][0];  
+        for (int i = 1; i < matrix.length; i++) {
+            if (matrix[i][i] < minElement) {
+                minElement = matrix[i][i];
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][matrix.length - i - 1] < minElement) {
+                minElement = matrix[i][matrix.length - i - 1];
+            }
+        }
+
+        return minElement;
+    }
+
+    private static int[][] removeRowsWithElement(int[][] matrix, int minDiagElement) {
+        boolean[] rowsToCopy = new boolean[matrix.length];
+        int[][] newMatrix;
+        int newNumberOfRows = matrix.length;
+        int currNewRow;
+
+        for (int i = 0; i < rowsToCopy.length; i++) {
+            rowsToCopy[i] = true;
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][j] == minDiagElement) {
+                    rowsToCopy[i] = false;
+                    newNumberOfRows--;
+                }
+            }
+        }
+
+        currNewRow = 0;
+        newMatrix = new int[newNumberOfRows][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            if (rowsToCopy[i]) {
+                newMatrix[currNewRow++] = Arrays.copyOf(matrix[i], matrix[i].length);
+            }
+        }
+
+        return newMatrix;
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                sb.append(String.format("%5d", matrix[i][j]));
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
